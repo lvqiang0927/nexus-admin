4521790ab6f9205bd73f34416c7878e769c0550d
lvq22 <lvq22@chinaunicom.cn>
2026-01-15 15:39:40 +0800

    初始化

---
+++ nexus-admin-backend/src/main/java/com/nexus/admin/NexusAdminBackendApplication.java
@@ -0,0 +1,12 @@
+package com.nexus.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusAdminBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexusAdminBackendApplication.class, args);
+    }
+}
